43bcf55d1fcffda0086cc4b729f2b034f15a74dd
WONGIL <realitsyourman@gmail.com>
2025-04-17 23:16:57 +0900

    feat: add register user method

--- src/main/java/com/part3/team07/sb01deokhugamteam07/controller/UserController.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/controller/UserController.java
@@ -1,11 +1,28 @@
 package com.part3.team07.sb01deokhugamteam07.controller;
 
+import com.part3.team07.sb01deokhugamteam07.dto.user.UserDto;
+import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserRegisterRequest;
+import com.part3.team07.sb01deokhugamteam07.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
+  private final UserService userService;
 
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public UserDto join(@Validated UserRegisterRequest request) {
+    userService.register(request);
+
+    return null;
+  }
 }
