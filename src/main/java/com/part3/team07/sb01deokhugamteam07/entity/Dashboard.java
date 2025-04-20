7aa1dc80c2e9bd77128af1eb8b6a22e92049f70b
kyungrin Kim <0123jade0123@gmail.com>
2025-04-20 23:07:54 +0900

    test: 파워 유저 커서 페이지네이션 레포지토리 단 테스트 추가

--- src/main/java/com/part3/team07/sb01deokhugamteam07/entity/Dashboard.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/entity/Dashboard.java
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Dashboard extends BaseEntity {
 
-  @Column(nullable = false)
+  @Column(name = "\"key\"", nullable = false)
   private UUID key;
 
   @Column(nullable = false)
@@ -32,7 +32,7 @@ public class Dashboard extends BaseEntity {
   @Column(nullable = false)
   private Period period;
 
-  @Column(nullable = false)
+  @Column(name = "\"value\"", nullable = false)
   private double value;
 
   @Enumerated(EnumType.STRING)
