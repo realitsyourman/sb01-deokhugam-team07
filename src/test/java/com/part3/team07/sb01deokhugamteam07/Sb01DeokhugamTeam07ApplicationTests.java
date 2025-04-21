0d41091e6b97fcc33253edc973b96159dba91e72
WONGIL <realitsyourman@gmail.com>
2025-04-21 16:17:18 +0900

    test: fix test

--- src/test/java/com/part3/team07/sb01deokhugamteam07/Sb01DeokhugamTeam07ApplicationTests.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/Sb01DeokhugamTeam07ApplicationTests.java
@@ -2,14 +2,8 @@ package com.part3.team07.sb01deokhugamteam07;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "jwt.security.jwt-secret=tRsLycvAv+NdFne3uTGzRHlH4XNXGP2v/6QW5dCT0Fs=",
-    "jwt.security.jwt-expiration=60000",
-    "jwt.security.jwt-issuer=test"
-})
 class Sb01DeokhugamTeam07ApplicationTests {
 
   @Test
