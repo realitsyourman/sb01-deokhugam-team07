381490fe6b3541810b340ff7f6f413f0ee89ea2d
reflash407 <reflash407@naver.com>
2025-04-22 03:43:01 +0900

    test : ReviewIntegrationTest 리뷰 생성 통합 테스트 추가

--- src/main/java/com/part3/team07/sb01deokhugamteam07/service/ReviewService.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/service/ReviewService.java
@@ -10,17 +10,19 @@ import com.part3.team07.sb01deokhugamteam07.mapper.ReviewMapper;
 import com.part3.team07.sb01deokhugamteam07.repository.BookRepository;
 import com.part3.team07.sb01deokhugamteam07.repository.ReviewRepository;
 import com.part3.team07.sb01deokhugamteam07.repository.UserRepository;
-import lombok.NoArgsConstructor;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ReviewService {
 
-    private ReviewRepository reviewRepository;
-    private BookRepository bookRepository;
-    private UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
 
+    @Transactional
     public ReviewDto create(ReviewCreateRequest request){
         if(reviewRepository.existsByUserIdAndBookId(request.userId(), request.bookId())){
             throw new IllegalArgumentException("이미 해당 도서에 대한 리뷰가 존재합니다.");
