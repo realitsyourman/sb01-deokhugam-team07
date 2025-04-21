399ad07e7507689c6c6c7ea0b94d09507f0fd98e
kyungrin Kim <0123jade0123@gmail.com>
2025-04-21 14:27:50 +0900

    test: 유저 점수에 따른 순위 할당 로직(assignUserRank) 테스트 추가

--- src/main/java/com/part3/team07/sb01deokhugamteam07/batch/poweruser/PowerUserDashboardBatchService.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/batch/poweruser/PowerUserDashboardBatchService.java
@@ -1,8 +1,14 @@
 package com.part3.team07.sb01deokhugamteam07.batch.poweruser;
 
+import com.part3.team07.sb01deokhugamteam07.entity.Dashboard;
+import com.part3.team07.sb01deokhugamteam07.entity.KeyType;
+import com.part3.team07.sb01deokhugamteam07.entity.Period;
 import com.part3.team07.sb01deokhugamteam07.entity.User;
+import com.part3.team07.sb01deokhugamteam07.entity.ValueType;
 import com.part3.team07.sb01deokhugamteam07.repository.UserRepository;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +22,7 @@ public class PowerUserDashboardBatchService {
   /**
    * 파워 유저 관련 데이터를 dashboard 테이블에 저장합니다.
    **/
-  public void savePowerUserDashboardData() {
+  public void savePowerUserDashboardData(Period period) {
     // 1. 전체 유저 조회 (is_deleted = false)
 
     // 2. 각 유저의 활동 정보 가져오기
@@ -26,19 +32,47 @@ public class PowerUserDashboardBatchService {
     // 2-4. 해당 기간 동안 댓글 단 수
     // 3. 활동 점수 계산
     // 4. 대시보드 데이터 구성
-
     // 5. SCORE 기준으로 전체 유저 순위 매기기 -> 메모리에서 정렬 후 rank 지정
+    // 6. 데이터베이스에 저장한다.
+
   }
 
   /**
    * 주어진 리뷰 점수, 좋아요 수, 댓글 수를 바탕으로 유저의 활동 점수를 계산하는 메서드.
    *
-   * @param reviewScore 유저의 리뷰 점수
-   * @param likeCount 유저가 받은 좋아요 수
+   * @param reviewScore  유저의 리뷰 점수
+   * @param likeCount    유저가 받은 좋아요 수
    * @param commentCount 유저가 작성한 댓글 수
    * @return 계산된 활동 점수
-   * **/
+   **/
   public double calculateScore(double reviewScore, int likeCount, int commentCount) {
     return (reviewScore * 0.5) + (likeCount * 0.2) + (commentCount * 0.3);
+  }
+
+
+  public List<Dashboard> assignUserRank(Map<UUID, Double> userScoreMap, Period period,
+      KeyType keyType, List<Dashboard> dashboards) {
+
+    List<Map.Entry<UUID, Double>> userScoreList = new ArrayList<>(userScoreMap.entrySet());
+
+    // Score 기준 내림차순으로 정렬
+    userScoreList.sort((user1, user2) -> user2.getValue().compareTo(user1.getValue()));
+
+    // 순위 계산 후 대시보드에 저장
+    int rank = 1;
+    for (Map.Entry<UUID, Double> user : userScoreList) {
+      UUID userId = user.getKey();
+      double score = user.getValue();
+
+      // 대시보드에 SCORE 가 ValueType 인 레코드에 rank 정보 추가하여 저장
+      dashboards.add(
+          new Dashboard(userId, keyType, period, score, ValueType.SCORE, rank)
+      );
+      
+      // rank 증가
+      rank++;
+    }
+
+    return dashboards;
   }
 }
--- src/test/java/com/part3/team07/sb01deokhugamteam07/batch/poweruser/PowerUserDashboardBatchServiceTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/batch/poweruser/PowerUserDashboardBatchServiceTest.java
@@ -1,13 +1,24 @@
 package com.part3.team07.sb01deokhugamteam07.batch.poweruser;
 
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.part3.team07.sb01deokhugamteam07.entity.Dashboard;
+import com.part3.team07.sb01deokhugamteam07.entity.KeyType;
+import com.part3.team07.sb01deokhugamteam07.entity.Period;
+import com.part3.team07.sb01deokhugamteam07.entity.ValueType;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class PowerUserDashboardBatchServiceTest {
@@ -17,7 +28,7 @@ class PowerUserDashboardBatchServiceTest {
 
   @Test
   @DisplayName("rank 의 기준이 되는 score 계산 로직")
-  void UserScoreCalculation() {
+  void UserScore_Calculation() {
     double reviewScore = 80;
     int likeCount = 10;
     int commentCount = 5;
@@ -27,5 +38,45 @@ class PowerUserDashboardBatchServiceTest {
     assertThat(score).isEqualTo(43.5);
   }
 
+  @Test
+  @DisplayName("score 정렬 후 rank 지정하기")
+  void AssignRank_BasedOnScore() {
+    UUID userId1 = UUID.randomUUID();
+    UUID userId2 = UUID.randomUUID();
+    UUID userId3 = UUID.randomUUID();
 
+    Map<UUID, Double> userScoreMap = new HashMap<>();
+
+    userScoreMap.put(userId1, 70.0);
+    userScoreMap.put(userId2, 90.0);
+    userScoreMap.put(userId3, 80.0);
+
+    Period period = Period.WEEKLY;
+    KeyType keyType = KeyType.USER;
+
+    List<Dashboard> dashboards = new ArrayList<>();
+
+    List<Dashboard> assignUserRank = powerUserDashboardBatchService.assignUserRank(userScoreMap,
+        period, keyType, dashboards);
+
+    assertThat(assignUserRank).hasSize(3);
+
+    ReflectionTestUtils.setField(assignUserRank.get(0), "id", userId2);
+    ReflectionTestUtils.setField(assignUserRank.get(1), "id", userId3);
+    ReflectionTestUtils.setField(assignUserRank.get(2), "id", userId1);
+
+    // 확인 용 Map
+    Map<UUID, Integer> expectedRanks = Map.of(
+        userId2, 1,
+        userId3, 2,
+        userId1, 3
+    );
+
+    for (Dashboard d : assignUserRank) {
+      UUID id = d.getId();
+      assertThat(d.getRank()).isEqualTo(expectedRanks.get(id));
+      assertThat(d.getValue()).isEqualTo(userScoreMap.get(id));
+      assertThat(d.getValueType()).isEqualTo(ValueType.SCORE);
+    }
+  }
 }
