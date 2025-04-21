6fed0c1c86d28f2a29f570c5657c333b4658d835
WONGIL <realitsyourman@gmail.com>
2025-04-21 14:22:58 +0900

    test: user login with Deokhugam-Request-User-ID response header - green

--- src/test/java/com/part3/team07/sb01deokhugamteam07/integration/UserServiceIntegrationTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/integration/UserServiceIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.part3.team07.sb01deokhugamteam07.integration;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -9,9 +8,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.part3.team07.sb01deokhugamteam07.dto.user.UserDto;
 import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserLoginRequest;
+import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserRegisterRequest;
 import com.part3.team07.sb01deokhugamteam07.service.UserService;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +17,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserServiceIntegrationTest {
@@ -36,28 +36,19 @@ public class UserServiceIntegrationTest {
   @Test
   @DisplayName("POST /api/users/login - Deokhugam-Request-User-ID 응답 헤더 확인")
   void checkUserLoginWithResponseHeader() throws Exception {
-    UserLoginRequest request = new UserLoginRequest("test@mail.com", "password1234");
-
-    UUID userId = UUID.randomUUID();
-    UserDto user = UserDto.builder()
-        .id(userId)
-        .email(request.email())
-        .nickname("user")
-        .createdAt(LocalDateTime.now())
-        .build();
-
-    when(userService.login(request))
-        .thenReturn(user);
+    UserRegisterRequest regReq = new UserRegisterRequest("test33@mail.com", "user", "password1234");
+    UserDto registered = userService.register(regReq);
+    UserLoginRequest request = new UserLoginRequest("test33@mail.com", "password1234");
 
     String requestJson = objectMapper.writeValueAsString(request);
-    String userJson = objectMapper.writeValueAsString(user);
+    String userJson = objectMapper.writeValueAsString(registered);
 
     mockMvc.perform(post("/api/users/login")
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestJson))
         .andExpect(status().isOk())
         .andExpect(content().json(userJson))
-        .andExpect(header().string("Deokhugam-Request-User-ID", userId.toString()))
-    ;
+        .andExpect(header().string("Deokhugam-Request-User-ID", registered.id().toString()));
+
   }
 }
