ecbdacdb579ac790dc1ae96e1a742aa803f390b4
reflash407 <reflash407@naver.com>
2025-04-22 03:13:57 +0900

    refactor : ReviewController 리뷰 생성 리펙터링

--- src/main/java/com/part3/team07/sb01deokhugamteam07/controller/ReviewController.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/controller/ReviewController.java
@@ -13,9 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/reviews")
@@ -25,24 +22,9 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<ReviewDto> create(@RequestBody @Valid ReviewCreateRequest request){
-        ReviewDto fakeReviewDto = new ReviewDto(
-                UUID.randomUUID(),                // id
-                UUID.randomUUID(),                // bookId
-                "fake-book-title",                // bookTitle
-                "fake-thumbnail-url",             // bookThumbnailUrl
-                UUID.randomUUID(),                // userId
-                "fake-nickname",                  // userNickName
-                "fake-content",                   // content
-                5,                                // rating
-                0,                                // likeCount
-                0,                                // commentCount
-                false,                            // likeByMe
-                LocalDateTime.now(),              // createdAt
-                LocalDateTime.now()               // updatedAt
-        );
-
+        ReviewDto reviewDto = reviewService.create(request);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(fakeReviewDto);
+                .body(reviewDto);
     }
 }
