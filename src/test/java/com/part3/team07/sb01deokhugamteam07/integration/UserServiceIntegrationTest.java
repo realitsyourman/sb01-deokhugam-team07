2dc89dbf30a7ea1ac6db0d65ca0780ccc80ba126
WONGIL <realitsyourman@gmail.com>
2025-04-21 15:49:17 +0900

    test: add property

--- src/test/java/com/part3/team07/sb01deokhugamteam07/integration/UserServiceIntegrationTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/integration/UserServiceIntegrationTest.java
@@ -16,12 +16,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+    "jwt.security.jwt-secret=tRsLycvAv+NdFne3uTGzRHlH4XNXGP2v/6QW5dCT0Fs=",
+    "jwt.security.jwt-expiration=60000",
+    "jwt.security.jwt-issuer=test"
+})
 public class UserServiceIntegrationTest {
 
   @Autowired
