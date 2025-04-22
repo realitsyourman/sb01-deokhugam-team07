81f02d7c93c4544583ce4f363ed09719525227c9
yxoni <psy337250@gmail.com>
2025-04-22 17:50:12 +0900

    test: 커버리지 보완

--- src/test/java/com/part3/team07/sb01deokhugamteam07/service/BookServiceTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/service/BookServiceTest.java
@@ -1,6 +1,7 @@
 package com.part3.team07.sb01deokhugamteam07.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -8,6 +9,7 @@ import static org.mockito.Mockito.verify;
 import com.part3.team07.sb01deokhugamteam07.dto.book.BookDto;
 import com.part3.team07.sb01deokhugamteam07.dto.book.request.BookCreateRequest;
 import com.part3.team07.sb01deokhugamteam07.entity.Book;
+import com.part3.team07.sb01deokhugamteam07.exception.book.DuplicateIsbnException;
 import com.part3.team07.sb01deokhugamteam07.mapper.BookMapper;
 import com.part3.team07.sb01deokhugamteam07.repository.BookRepository;
 import java.time.LocalDate;
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -45,64 +48,92 @@ class BookServiceTest {
   private LocalDate publishedDate;
   private String isbn;
   private Book book;
-  private BookDto bookDto;
 
   @BeforeEach
   void setUp() {
     id = UUID.randomUUID();
-    title = "홍길동전";
-    author = "허균";
-    description = "고전 소설";
-    publisher = "출판사";
+    title = "title";
+    author = "author";
+    description = "description";
+    publisher = "publisher";
     publishedDate = LocalDate.of(1618, 1, 1);
 
     book = new Book(title, author, description, publisher, publishedDate,
         isbn, "", 0, 0);
-
-    bookDto = new BookDto(
-        id,
-        title,
-        author,
-        description,
-        publisher,
-        publishedDate,
-        "",
-        "",
-        0,
-        0,
-        LocalDateTime.now(),
-        LocalDateTime.now()
-    );
   }
 
-  @Test
-  @DisplayName("도서 생성 성공")
-  void create() {
-    // given
-    BookCreateRequest request = new BookCreateRequest(
-        title,
-        author,
-        description,
-        publisher,
-        publishedDate,
-        ""
-    );
+  @Nested
+  @DisplayName("도서 생성")
+  class CreateTest {
+    @Test
+    @DisplayName("도서 생성 성공")
+    void create_success() {
+      // given
+      BookCreateRequest request = new BookCreateRequest(
+          title,
+          author,
+          description,
+          publisher,
+          publishedDate,
+          ""
+      );
 
-    given(thumbnailImageService.save(any())).willReturn("");
-    given(bookRepository.save(any(Book.class))).will(invocation -> {
-      Book book = invocation.getArgument(0);
-      ReflectionTestUtils.setField(book, "id", id);
-      return book;
-    });
-    given(bookMapper.toDto(any(Book.class))).willReturn(bookDto);
+      BookDto bookDto = new BookDto(
+          id,
+          title,
+          author,
+          description,
+          publisher,
+          publishedDate,
+          "",
+          "",
+          0,
+          0,
+          LocalDateTime.now(),
+          LocalDateTime.now()
+      );
 
-    // when
-    BookDto result = bookService.create(request, null);
+      given(bookRepository.existsByIsbn(request.isbn())).willReturn(false);
+      given(thumbnailImageService.save(any())).willReturn("");
+      given(bookRepository.save(any(Book.class))).will(invocation -> {
+        Book book = invocation.getArgument(0);
+        ReflectionTestUtils.setField(book, "id", id);
+        return book;
+      });
+      given(bookMapper.toDto(any(Book.class))).willReturn(bookDto);
 
-    // then
-    assertThat(result).isEqualTo(bookDto);
-    verify(thumbnailImageService).save(any());
-    verify(bookRepository).save(any(Book.class));
+      // when
+      BookDto result = bookService.create(request, null);
+
+      // then
+      assertThat(result).isEqualTo(bookDto);
+      verify(thumbnailImageService).save(any());
+      verify(bookRepository).save(any(Book.class));
+    }
+
+    @Test
+    @DisplayName("도서 생성 실패 - 중복 ISBN")
+    void create_fail_duplicateIsbn() {
+      // given
+      BookCreateRequest request = new BookCreateRequest(
+          title,
+          author,
+          description,
+          publisher,
+          publishedDate,
+          ""
+      );
+
+      given(bookRepository.existsByIsbn(request.isbn())).willReturn(true);
+
+      // when & then
+      assertThrows(DuplicateIsbnException.class,
+          () -> bookService.create(request, null)
+      );
+
+    }
   }
+
+
 
 }
