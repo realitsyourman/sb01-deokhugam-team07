5fffb11b5e9793dd6498874cc1632383153ad2a9
yxoni <psy337250@gmail.com>
2025-04-22 11:32:08 +0900

    test: 주석 제거

--- src/test/java/com/part3/team07/sb01deokhugamteam07/Sb01DeokhugamTeam07ApplicationTests.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/Sb01DeokhugamTeam07ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Sb01DeokhugamTeam07ApplicationTests {
 
-//  @Test
-//  void contextLoads() {
-//  }
+  @Test
+  void contextLoads() {
+  }
 
 }
