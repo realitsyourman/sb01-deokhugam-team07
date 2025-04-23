bd894fda5e4baa74133be2e0bffbc133487f1c0b
kyungrin Kim <0123jade0123@gmail.com>
2025-04-23 15:32:13 +0900

    test: 인기 도서 대시보드 저장 로직 기초 테스트및 구현

---
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/batch/poweruser/PopularBookDashboardBatchService.java
@@ -0,0 +1,92 @@
+package com.part3.team07.sb01deokhugamteam07.batch.poweruser;
+
+import com.part3.team07.sb01deokhugamteam07.entity.Book;
+import com.part3.team07.sb01deokhugamteam07.entity.Dashboard;
+import com.part3.team07.sb01deokhugamteam07.entity.KeyType;
+import com.part3.team07.sb01deokhugamteam07.entity.Period;
+import com.part3.team07.sb01deokhugamteam07.entity.Review;
+import com.part3.team07.sb01deokhugamteam07.repository.BookRepository;
+import com.part3.team07.sb01deokhugamteam07.repository.DashboardRepository;
+import com.part3.team07.sb01deokhugamteam07.repository.ReviewRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PopularBookDashboardBatchService {
+
+  private final BookRepository bookRepository;
+  private final DateRangeUtil dateRangeUtil;
+  private final ReviewRepository reviewRepository;
+  private final AssignRankUtil assignRankUtil;
+  private final DashboardRepository dashboardRepository;
+
+
+  /**
+   * 인기 도서 관련 데이터를 dashboard 테이블에 저장합니다.
+   *
+   * @param period 기간 정보 (e.g. DAILY, WEEKLY, MONTHLY, ALL_TIME)
+   **/
+  public void savePopularBookDashboardData(Period period) {
+    // 1. 전체 도서 조회 (is_deleted = false)
+    List<Book> books = bookRepository.findByIsDeletedFalseOrderByCreatedAtAsc();
+
+    // 날짜 범위 계산
+    LocalDate[] dateRange = dateRangeUtil.getDateRange(period);
+    LocalDateTime startDateTime = dateRange[0].atStartOfDay();
+    LocalDateTime endDateTime = dateRange[1].plusDays(1).atStartOfDay().minusNanos(1);
+
+    // 결과 저장용 대시보드 리스트
+    List<Dashboard> dashboards = new ArrayList<>();
+    Map<UUID, Double> bookScoreMap = new LinkedHashMap<>();
+
+    // 2. 각 도서의 정보 가져오기
+    for (Book book : books) {
+      UUID bookId = book.getId();
+
+      List<Review> reviews = reviewRepository.findByBookIdAndCreatedAtBetweenAndIsDeletedFalse(
+          bookId,
+          startDateTime,
+          endDateTime
+      );
+
+      // 2-1. 해당 기간 동안의 리뷰 수
+      int reviewCount = reviews.size();
+
+      // 2-2. 해당 기간 동안받은 평점의 평균
+      double ratingAverage = reviews.stream()
+          .mapToInt(Review::getRating)
+          .average()
+          .orElse(0.0);
+
+      // 3. 점수 계산
+      double score = calculateScore(reviewCount, ratingAverage);
+      bookScoreMap.put(bookId, score);
+    }
+
+    // 4. Score 기준으로 전체 도서 순위 매기기 -> 정렬 후 rank 지정
+    dashboards = assignRankUtil.assignRank(bookScoreMap, period, KeyType.BOOK, dashboards);
+
+    // 5. 데이터베이스에 저장
+    dashboardRepository.saveAll(dashboards);
+  }
+
+  /**
+   * 리뷰 수, 평점 평균을 바탕으로 도서의 점수를 계산하는 메서드. 점수 = (해당 기간의 리뷰수 * 0.4) + (해당 기간의 평점 평균 * 0.6)
+   *
+   * @param reviewCount   도서에 작성된 리뷰 수
+   * @param ratingAverage 해당 기간의 평점 평균
+   * @return 계산된 활동 점수
+   **/
+  private double calculateScore(int reviewCount, double ratingAverage) {
+    return (reviewCount * 0.4) + (ratingAverage * 0.6);
+  }
+
+}
--- src/main/java/com/part3/team07/sb01deokhugamteam07/repository/BookRepository.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/repository/BookRepository.java
@@ -2,10 +2,14 @@ package com.part3.team07.sb01deokhugamteam07.repository;
 
 
 import com.part3.team07.sb01deokhugamteam07.entity.Book;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
   boolean existsByIsbn(String isbn);
+
+  List<Book> findByIsDeletedFalseOrderByCreatedAtAsc();
 
 }
