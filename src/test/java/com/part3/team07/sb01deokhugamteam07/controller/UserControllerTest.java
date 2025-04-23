a55ff9b707740136a45f4f90bfa11fda4e60cb55
WONGIL <realitsyourman@gmail.com>
2025-04-23 11:20:05 +0900

    test: controller - login user

--- src/test/java/com/part3/team07/sb01deokhugamteam07/controller/UserControllerTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/controller/UserControllerTest.java
@@ -10,12 +10,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.part3.team07.sb01deokhugamteam07.config.SecurityConfig;
 import com.part3.team07.sb01deokhugamteam07.dto.user.UserDto;
+import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserLoginRequest;
 import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserRegisterRequest;
 import com.part3.team07.sb01deokhugamteam07.exception.user.DuplicateUserEmailException;
+import com.part3.team07.sb01deokhugamteam07.exception.user.IllegalUserPasswordException;
+import com.part3.team07.sb01deokhugamteam07.exception.user.UserNotFoundException;
 import com.part3.team07.sb01deokhugamteam07.repository.UserRepository;
 import com.part3.team07.sb01deokhugamteam07.security.CustomUserDetailsService;
 import com.part3.team07.sb01deokhugamteam07.service.UserService;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +27,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -41,6 +47,9 @@ class UserControllerTest {
 
   @MockitoBean
   private UserRepository userRepository;
+
+  @MockitoBean
+  private AuthenticationManager authenticationManager;
 
   @Test
   @DisplayName("POST /api/users - 회원가입 성공")
@@ -92,11 +101,6 @@ class UserControllerTest {
     UserRegisterRequest request = new UserRegisterRequest("testmail.com", "test", "11");
     String requestJson = objectMapper.writeValueAsString(request);
 
-    UUID userId = UUID.randomUUID();
-    UserDto response = new UserDto(userId, "testmail.com", "test", LocalDateTime.now());
-    String responseJson = objectMapper.writeValueAsString(response);
-
-
     // then
     mockMvc
         .perform(post("/api/users")
@@ -105,6 +109,77 @@ class UserControllerTest {
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.email").value("testmail.com"))
         .andExpect(jsonPath("$.nickname").value("test"))
+    ;
+  }
+
+  @Test
+  @DisplayName("POST /api/users/login - 로그인")
+  void login() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest("test@mail.com", "password123");
+    String requestJson = objectMapper.writeValueAsString(request);
+
+    UUID userId = UUID.randomUUID();
+    UserDto response = new UserDto(userId, "testmail.com", "test", LocalDateTime.now());
+    String responseJson = objectMapper.writeValueAsString(response);
+
+    // when
+    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+        .thenReturn(new UsernamePasswordAuthenticationToken(
+            request.email(),
+            null,
+            List.of()
+        ));
+    when(userService.login(any(UserLoginRequest.class)))
+        .thenReturn(response);
+
+    // then
+    mockMvc
+        .perform(post("/api/users/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestJson))
+        .andExpect(status().isOk())
+        .andExpect(content().json(responseJson))
+    ;
+  }
+
+  @Test
+  @DisplayName("POST /api/users/login - 로그인 실퍠(존재하지 않는 유저)")
+  void failLoginCauseNotFoundUser() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest("test@mail.com", "password123");
+    String requestJson = objectMapper.writeValueAsString(request);
+
+    // when
+    when(userService.login(any(UserLoginRequest.class)))
+        .thenThrow(new UserNotFoundException(request));
+
+    // then
+    mockMvc
+        .perform(post("/api/users/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestJson))
+        .andExpect(status().isNotFound())
+    ;
+  }
+
+  @Test
+  @DisplayName("POST /api/users/login - 로그인 실퍠(존재하지 않는 유저)")
+  void failLoginCauseInvalidPassword() throws Exception {
+    // given
+    UserLoginRequest request = new UserLoginRequest("test@mail.com", "password123");
+    String requestJson = objectMapper.writeValueAsString(request);
+
+    // when
+    when(userService.login(any(UserLoginRequest.class)))
+        .thenThrow(new IllegalUserPasswordException(request));
+
+    mockMvc
+        .perform(post("/api/users/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requestJson))
+        .andExpect(status().is4xxClientError())
+        .andExpect(jsonPath("$.email").value("test@mail.com"))
     ;
   }
 }
