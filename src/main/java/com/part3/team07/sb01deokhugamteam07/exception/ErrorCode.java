ad587b40e454c7316438b494d7db93fcdc419271
LeeSG-0114 <61682044+LeeSG-0114@users.noreply.github.com>
2025-04-23 09:13:24 +0900

    fix: fix ErrorCode

--- src/main/java/com/part3/team07/sb01deokhugamteam07/exception/ErrorCode.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/exception/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
   THUMBNAIL_IMAGE_ALREADY_EXISTS("이미 존재하는 썸네일 이미지입니다."),
   THUMBNAIL_IMAGE_NOT_FOUND("해당 썸네일 이미지가 존재하지 않습니다."),
   THUMBNAIL_IMAGE_STORAGE_INIT("썸네일 저장소 초기화 중 오류가 발생하였습니다."),
-  THUMBNAIL_IMAGE_STORAGE("썸네일 이미지 저장 중 오류가 발생하였습니다.");
+  THUMBNAIL_IMAGE_STORAGE("썸네일 이미지 저장 중 오류가 발생하였습니다."),
 
   //Comment
   COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다"),
