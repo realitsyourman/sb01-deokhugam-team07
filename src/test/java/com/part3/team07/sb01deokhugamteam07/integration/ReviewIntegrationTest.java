2194f6f30971133e5e44a184fb2dd7900d20310e
reflash407 <reflash407@naver.com>
2025-04-24 02:08:11 +0900

    test : ReviewIntegrationTest hardDelete 통합 테스트 추가

--- src/test/java/com/part3/team07/sb01deokhugamteam07/integration/ReviewIntegrationTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/integration/ReviewIntegrationTest.java
@@ -198,7 +198,7 @@ public class ReviewIntegrationTest {
 
     @DisplayName("리뷰 논리 삭제 api 통합 테스트")
     @Test
-    void test() throws Exception {
+    void softDelete() throws Exception {
         //given
         User user = User.builder()
                 .nickname("user")
@@ -233,6 +233,46 @@ public class ReviewIntegrationTest {
         //when then
         mockMvc.perform(delete("/api/reviews/{reviewId}", review.getId())
                 .header("Deokhugam-Request-User-ID", user.getId().toString()))
-                .andExpect(status().isOk());
+                .andExpect(status().isNoContent());
+    }
+
+    @DisplayName("리뷰 물리 삭제 api 통합 테스트")
+    @Test
+    void hardDelete() throws Exception {
+        //given
+        User user = User.builder()
+                .nickname("user")
+                .email("user@abc.com")
+                .password("user1234")
+                .build();
+        userRepository.save(user);
+
+        Book book = Book.builder()
+                .title("Book")
+                .author("Author")
+                .description("Description")
+                .publisher("Publisher")
+                .publishDate(LocalDate.now())
+                .isbn("1234567890123")
+                .thumbnailFileName("Url")
+                .reviewCount(0)
+                .rating(0.0)
+                .build();
+        bookRepository.save(book);
+
+        Review review = Review.builder()
+                .user(user)
+                .book(book)
+                .content("정말 좋은 책입니다.")
+                .rating(5)
+                .likeCount(0)
+                .commentCount(0)
+                .build();
+        reviewRepository.save(review);
+
+        //when then
+        mockMvc.perform(delete("/api/reviews/{reviewId}/hard", review.getId())
+                .header("Deokhugam-Request-User-ID", user.getId().toString()))
+                .andExpect(status().isNoContent());
     }
 }
