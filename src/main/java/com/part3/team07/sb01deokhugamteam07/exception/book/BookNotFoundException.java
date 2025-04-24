7b2be50d1d8ac83c931f9e6db81ede31b2a79dd3
yxoni <psy337250@gmail.com>
2025-04-24 14:07:48 +0900

    refacotr: BookService에 적용된 커스텀 예외 고도화

--- src/main/java/com/part3/team07/sb01deokhugamteam07/exception/book/BookNotFoundException.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/exception/book/BookNotFoundException.java
@@ -1,7 +1,7 @@
 package com.part3.team07.sb01deokhugamteam07.exception.book;
 
-import com.part3.team07.sb01deokhugamteam07.dto.notification.UUID;
 import com.part3.team07.sb01deokhugamteam07.exception.ErrorCode;
+import java.util.UUID;
 
 public class BookNotFoundException extends BookException {
 
--- src/main/java/com/part3/team07/sb01deokhugamteam07/service/BookService.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/service/BookService.java
@@ -4,17 +4,14 @@ import com.part3.team07.sb01deokhugamteam07.dto.book.BookDto;
 import com.part3.team07.sb01deokhugamteam07.dto.book.request.BookCreateRequest;
 import com.part3.team07.sb01deokhugamteam07.dto.book.request.BookUpdateRequest;
 import com.part3.team07.sb01deokhugamteam07.entity.Book;
+import com.part3.team07.sb01deokhugamteam07.exception.book.BookAlreadyExistsException;
 import com.part3.team07.sb01deokhugamteam07.exception.book.BookNotFoundException;
-import com.part3.team07.sb01deokhugamteam07.exception.book.DuplicateIsbnException;
 import com.part3.team07.sb01deokhugamteam07.mapper.BookMapper;
 import com.part3.team07.sb01deokhugamteam07.repository.BookRepository;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,7 +28,7 @@ public class BookService {
   public BookDto create(BookCreateRequest request,
       MultipartFile thumbnailImage) {
     if (bookRepository.existsByIsbn(request.isbn())) {
-      throw new DuplicateIsbnException();
+      throw BookAlreadyExistsException.withIsbn(request.isbn());
     }
     String thumbnailFileName = thumbnailImageService.save(thumbnailImage);
 
@@ -51,7 +48,7 @@ public class BookService {
 
   public BookDto update(UUID id, BookUpdateRequest request) {
     Book book = bookRepository.findById(id)
-        .orElseThrow(() -> new BookNotFoundException());
+        .orElseThrow(() -> BookNotFoundException.withId(id));
 
     Optional.ofNullable(request.title()).ifPresent(book::updateTitle);
     Optional.ofNullable(request.author()).ifPresent(book::updateAuthor);
@@ -64,7 +61,7 @@ public class BookService {
 
   public void softDelete(UUID id) {
     Book book = bookRepository.findById(id)
-        .orElseThrow(() -> new BookNotFoundException());
+        .orElseThrow(() -> BookNotFoundException.withId(id));
 
     book.softDelete();
     // TODO: 관련 엔티티 논리 삭제
@@ -72,7 +69,7 @@ public class BookService {
 
   public void hardDelete(UUID id) {
     if (!bookRepository.existsById(id)) {
-      throw new BookNotFoundException();
+      throw BookNotFoundException.withId(id);
     }
 
     bookRepository.deleteById(id);
