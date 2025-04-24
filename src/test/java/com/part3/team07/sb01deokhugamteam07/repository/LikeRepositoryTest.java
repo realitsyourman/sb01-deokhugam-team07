931bccabdd3ed3562559076d9aaf61003bd0a325
reflash407 <reflash407@naver.com>
2025-04-24 14:33:42 +0900

    test : LikeRepositoryTest findByReviewIdAndUserId 테스트 추가

--- src/test/java/com/part3/team07/sb01deokhugamteam07/repository/LikeRepositoryTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/repository/LikeRepositoryTest.java
@@ -1,9 +1,58 @@
 package com.part3.team07.sb01deokhugamteam07.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.part3.team07.sb01deokhugamteam07.entity.Like;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@EnableJpaAuditing
+@ActiveProfiles("test")
 class LikeRepositoryTest {
 
+    @Autowired
+    private ReviewRepository reviewRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private LikeRepository likeRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @DisplayName("리뷰 ID와 유저 ID로 좋아요를 조회할 수 있다")
+    @Test
+    void findByReviewIdAndUserId() {
+        //given
+        UUID reviewId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        Like like = Like.builder()
+                .reviewId(reviewId)
+                .userId(userId)
+                .build();
+        likeRepository.save(like);
+
+        //when
+        Optional<Like> result = likeRepository.findByReviewIdAndUserId(reviewId, userId);
+
+        //then
+        assertThat(result).isPresent();
+        assertThat(result.get().getReviewId()).isEqualTo(reviewId);
+    }
 
 }
