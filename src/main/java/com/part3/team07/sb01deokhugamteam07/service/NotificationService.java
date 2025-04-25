731778ae151c64e6f1d0ee1157570a2be6ff14d7
kyungrin Kim <0123jade0123@gmail.com>
2025-04-25 11:09:23 +0900

    refactor: 알림 생성 코드 try-catch 리팩토링 및 테스트 추가

--- src/main/java/com/part3/team07/sb01deokhugamteam07/service/NotificationService.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/service/NotificationService.java
@@ -34,32 +34,34 @@ public class NotificationService {
    * @param request 알림 생성 관련 정보를 담은 요청 객체
    **/
   public void create(NotificationCreateRequest request) {
-    log.info("알림 생성 시작");
+    try {
+      log.info("알림 생성 시작");
+      User sender = userRepository.findById(request.getSenderId())
+          .orElseThrow(() -> new UserNotFoundException(request.getSenderId()));
 
-    User sender = userRepository.findById(request.getSenderId())
-        .orElseThrow(() -> new UserNotFoundException(request.getSenderId()));
+      Review review = reviewRepository.findById(request.getReviewId())
+          .orElseThrow(() -> new NoSuchElementException(String.valueOf(request.getReviewId())));
 
-    Review review = reviewRepository.findById(request.getReviewId())
-        .orElseThrow(()-> new NoSuchElementException(String.valueOf(request.getReviewId())));
+      User receiver = review.getUser();
 
-    User receiver = review.getUser();
+      String content = switch (request.getType()) {
+        case REVIEW_LIKED -> "[" + sender.getNickname() + "]" + "님이 나의 리뷰를 좋아합니다.";
+        case REVIEW_COMMENTED -> "[" + sender.getNickname() + "]" + "님이 나의 리뷰에 댓글을 남겼습니다.";
+        case REVIEW_RANKED ->
+            "나의 리뷰가" + request.getPeriod() + " 인기 리뷰" + request.getRank() + "위에 선정되었습니다.";
+      };
 
-    String content = switch (request.getType()) {
-      case REVIEW_LIKED -> "[" + sender.getNickname() + "]" + "님이 나의 리뷰를 좋아합니다.";
-      case REVIEW_COMMENTED -> "[" + sender.getNickname() + "]" + "님이 나의 리뷰에 댓글을 남겼습니다.";
-      case REVIEW_RANKED ->
-          "나의 리뷰가" + request.getPeriod() + " 인기 리뷰" + request.getRank() + "위에 선정되었습니다.";
-    };
+      Notification notification = Notification.builder()
+          .userId(receiver.getId())
+          .reviewId(request.getReviewId())
+          .content(content)
+          .confirmed(false)
+          .build();
 
-    Notification notification = Notification.builder()
-        .userId(receiver.getId())
-        .reviewId(request.getReviewId())
-        .content(content)
-        .confirmed(false)
-        .build();
-
-    notificationRepository.save(notification);
-    log.info("알림 생성 완료");
+      notificationRepository.save(notification);
+      log.info("알림 생성 완료");
+    }catch (Exception e){
+      log.warn("알림 생성 실패: {}", e.getMessage(), e);
+    }
   }
-
 }
--- src/test/java/com/part3/team07/sb01deokhugamteam07/service/NotificationServiceTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/service/NotificationServiceTest.java
@@ -1,5 +1,6 @@
 package com.part3.team07.sb01deokhugamteam07.service;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -14,13 +15,14 @@ import com.part3.team07.sb01deokhugamteam07.repository.ReviewRepository;
 import com.part3.team07.sb01deokhugamteam07.repository.UserRepository;
 import java.util.Optional;
 import java.util.UUID;
-import javax.swing.text.html.Option;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,21 +39,31 @@ class NotificationServiceTest {
 
   @InjectMocks
   private NotificationService notificationService;
-  
-  @Test
-  @DisplayName("알림 생성 성공")
-  void create_Success_When_Review_Is_Liked(){
-    UUID senderId = UUID.randomUUID();
-    User sender = User.builder().nickname("testUser").build();
+
+  private UUID senderId;
+  private User sender;
+  private UUID receiverId;
+  private User receiver;
+  private UUID reviewId;
+  private Review review;
+
+  @BeforeEach
+  void setup(){
+    senderId = UUID.randomUUID();
+    sender = User.builder().nickname("testUser").build();
     ReflectionTestUtils.setField(sender, "id", senderId);
 
-    UUID receiverId = UUID.randomUUID();
-    User receiver = User.builder().build();
+    receiverId = UUID.randomUUID();
+    receiver = User.builder().build();
     ReflectionTestUtils.setField(receiver, "id", receiverId);
 
-    UUID reviewId = UUID.randomUUID();
-    Review review = Review.builder().user(receiver).build();
+    reviewId = UUID.randomUUID();
+    review = Review.builder().user(receiver).build();
+  }
 
+  @Test
+  @DisplayName("알림 생성 성공 : 좋아요")
+  void create_Success_When_Review_is_Liked() {
     NotificationCreateRequest request = NotificationCreateRequest.builder()
         .type(NotificationType.REVIEW_LIKED)
         .senderId(senderId)
@@ -67,5 +79,62 @@ class NotificationServiceTest {
     verify(userRepository).findById(any());
     verify(reviewRepository).findById(any());
     verify(notificationRepository).save(any(Notification.class));
+  }
+
+  @Test
+  @DisplayName("알림 생성 성공 : 댓글")
+  void create_Success_When_Review_is_Commented() {
+    NotificationCreateRequest request = NotificationCreateRequest.builder()
+        .type(NotificationType.REVIEW_COMMENTED)
+        .senderId(senderId)
+        .reviewId(reviewId)
+        .build();
+
+    when(userRepository.findById(any(UUID.class))).thenReturn(Optional.of(sender));
+    when(reviewRepository.findById(any(UUID.class))).thenReturn(Optional.of(review));
+
+    // when
+    notificationService.create(request);
+
+    verify(userRepository).findById(any());
+    verify(reviewRepository).findById(any());
+    verify(notificationRepository).save(any(Notification.class));
+  }
+
+  @Test
+  @DisplayName("알림 생성 성공 : 순위")
+  void create_Success_When_Review_is_Ranked() {
+    NotificationCreateRequest request = NotificationCreateRequest.builder()
+        .type(NotificationType.REVIEW_COMMENTED)
+        .senderId(senderId)
+        .reviewId(reviewId)
+        .build();
+
+    when(userRepository.findById(any(UUID.class))).thenReturn(Optional.of(sender));
+    when(reviewRepository.findById(any(UUID.class))).thenReturn(Optional.of(review));
+
+    // when
+    notificationService.create(request);
+
+    verify(userRepository).findById(any());
+    verify(reviewRepository).findById(any());
+    verify(notificationRepository).save(any(Notification.class));
+  }
+
+  @Test
+  @DisplayName("알림 생성 실패 : 오류 발생시 이외의 프로레스에 예외가 전파되지 않게 한다")
+  void create_Fail_When_Review_Is_Liked() {
+
+    NotificationCreateRequest request = NotificationCreateRequest.builder()
+        .type(NotificationType.REVIEW_LIKED)
+        .senderId(senderId)
+        .reviewId(reviewId)
+        .build();
+
+    when(userRepository.findById(any(UUID.class))).thenReturn(Optional.of(sender));
+    when(reviewRepository.findById(any(UUID.class))).thenReturn(Optional.of(review));
+    when(notificationRepository.save(any(Notification.class))).thenThrow(new DataAccessResourceFailureException("DB 연결 실패"));
+
+    assertDoesNotThrow(() -> notificationService.create(request));
   }
 }
