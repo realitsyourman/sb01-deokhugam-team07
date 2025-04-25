3b3ebdcf427f127469fc4401667fb906913c1dbc
LeeSG-0114 <leesin0114@naver.com>
2025-04-25 18:09:24 +0900

    chore: 레포지토리 테스트들에 QuerydslConfig import, QuerydslConfig에 EntityManager추가
    # Conflicts:
    #	src/test/java/com/part3/team07/sb01deokhugamteam07/repository/DashboardRepositoryCustomImplTest.java

--- src/test/java/com/part3/team07/sb01deokhugamteam07/repository/ReviewRepositoryTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/repository/ReviewRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.part3.team07.sb01deokhugamteam07.repository;
 
+import com.part3.team07.sb01deokhugamteam07.config.QuerydslConfig;
 import com.part3.team07.sb01deokhugamteam07.entity.Book;
 import com.part3.team07.sb01deokhugamteam07.entity.Review;
 import com.part3.team07.sb01deokhugamteam07.entity.User;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -20,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EnableJpaAuditing
+@Import(QuerydslConfig.class)
 @ActiveProfiles("test")
 class ReviewRepositoryTest {
 
