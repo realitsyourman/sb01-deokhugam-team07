42020ccf70c991bd2d5bee8355ed5834f186dbb3
kyungrin Kim <0123jade0123@gmail.com>
2025-04-28 17:07:09 +0900

    test: UserController 에 파워 유저 조회하는 API 관련 테스트및 기초 구현 추가

--- src/test/java/com/part3/team07/sb01deokhugamteam07/controller/UserControllerTest.java
+++ src/test/java/com/part3/team07/sb01deokhugamteam07/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -19,12 +20,15 @@ import com.part3.team07.sb01deokhugamteam07.dto.user.UserDto;
 import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserLoginRequest;
 import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserRegisterRequest;
 import com.part3.team07.sb01deokhugamteam07.dto.user.request.UserUpdateRequest;
+import com.part3.team07.sb01deokhugamteam07.dto.user.response.CursorPageResponsePowerUserDto;
+import com.part3.team07.sb01deokhugamteam07.entity.Period;
 import com.part3.team07.sb01deokhugamteam07.entity.User;
 import com.part3.team07.sb01deokhugamteam07.exception.user.DuplicateUserEmailException;
 import com.part3.team07.sb01deokhugamteam07.exception.user.IllegalUserPasswordException;
 import com.part3.team07.sb01deokhugamteam07.exception.user.UserNotFoundException;
 import com.part3.team07.sb01deokhugamteam07.repository.UserRepository;
 import com.part3.team07.sb01deokhugamteam07.security.CustomUserDetailsService;
+import com.part3.team07.sb01deokhugamteam07.service.DashboardService;
 import com.part3.team07.sb01deokhugamteam07.service.UserService;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -38,6 +42,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
@@ -60,6 +65,9 @@ class UserControllerTest {
 
   @MockitoBean
   private AuthenticationManager authenticationManager;
+
+  @MockitoBean
+  private DashboardService dashboardService;
 
   @Test
   @DisplayName("POST /api/users - 회원가입 성공")
@@ -463,5 +471,33 @@ class UserControllerTest {
         .perform(delete("/api/users/{userId}/hard", userId)
             .header("Deokhugam-Request-User-ID", userId))
         .andExpect(status().isNotFound());
+  }
+  
+  @Test
+  @WithMockUser
+  @DisplayName("GET /api/users/power - 파워 유저 조회")
+  void findPowerUsers() throws Exception {
+    Period period = Period.DAILY;
+    String direction = "asc";
+    String cursor = null;
+    String after = null;
+    int limit = 50;
+
+    CursorPageResponsePowerUserDto cursorPageResponsePowerUserDto =
+        CursorPageResponsePowerUserDto.builder()
+            .hasNext(false)
+            .build();
+
+    when(dashboardService.getPowerUsers(period,direction,cursor,after,limit)).thenReturn(cursorPageResponsePowerUserDto);
+
+    mockMvc.perform(get("/api/users/power")
+            .param("period", period.toString())
+            .param("direction",direction)
+            .param("cursor", cursor)
+            .param("after", after)
+            .param("limit", String.valueOf(limit))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.hasNext" ).value(false));
   }
 }
