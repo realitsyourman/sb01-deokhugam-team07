ac63f2346151092820f8a9a10dd10c3a5d6bceae
reflash407 <reflash407@naver.com>
2025-04-28 09:55:15 +0900

    feat : ErrorCode 리뷰 관련 에러 코드 추가

--- src/main/java/com/part3/team07/sb01deokhugamteam07/exception/ErrorCode.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/exception/ErrorCode.java
@@ -17,7 +17,14 @@ public enum ErrorCode {
 
   //Comment
   COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다"),
-  COMMENT_UNAUTHORIZED("권한이 없습니다");
+  COMMENT_UNAUTHORIZED("권한이 없습니다"),
+
+  //Review 관련 에러 코드
+  REVIEW_NOT_FOUND("리뷰 정보를 찾을 수 없습니다."),
+  DUPLICATE_REVIEW_EXISTS("이미 작성한 리뷰가 존재합니다."),
+  REVIEW_UNAUTHORIZED_DELETE("리뷰 삭제 권한이 없습니다."),
+  REVIEW_UNAUTHORIZED_UPDATE("리뷰 수정 권한이 없습니다."),
+  INVALID_REVIEW_REQUEST("리뷰 요청이 잘못되었습니다."); // (정렬 기준 오류, 페이지네이션 오류 등 포함)
 
   private final String message;
 
