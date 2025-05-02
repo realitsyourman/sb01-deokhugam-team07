1fef9911d11323e1675c6cf46e67ac2c1786fb54
reflash407 <reflash407@naver.com>
2025-05-02 10:32:43 +0900

    feat : ReviewRepositoryCustom 추가

---
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/repository/ReviewRepositoryCustom.java
@@ -0,0 +1,14 @@
+package com.part3.team07.sb01deokhugamteam07.repository;
+
+import com.part3.team07.sb01deokhugamteam07.type.ReviewDirection;
+import com.part3.team07.sb01deokhugamteam07.type.ReviewOrderBy;
+import com.querydsl.core.Tuple;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface ReviewRepositoryCustom {
+    List<Tuple> findAll(UUID userId, UUID bookId, String keyword, ReviewOrderBy orderBy, ReviewDirection direction,
+                        String cursor, LocalDateTime after, int limit, UUID requestUserId);
+}
