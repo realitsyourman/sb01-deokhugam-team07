d46842b3f4d17cc1800210b98992cebfeb426391
WONGIL <realitsyourman@gmail.com>
2025-05-02 13:43:16 +0900

    fix: add security endpoint

--- src/main/java/com/part3/team07/sb01deokhugamteam07/config/SecurityConfig.java
+++ src/main/java/com/part3/team07/sb01deokhugamteam07/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
                 "/error",
                 "/api/users",
                 "/swagger-ui/**",
-                "/v3/api-docs/**"
+                "/v3/api-docs/**",
+                "/api/books/popular"
             ).permitAll() // 위의 엔드포인트 허용
             .requestMatchers("/api/**").authenticated() // 나머지 엔드포인트 막음
         )
